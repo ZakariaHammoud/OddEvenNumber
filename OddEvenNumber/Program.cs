d1fe4d9dcd6ce4fde98a94674a327e6f388e7c14
Zakaria Hammoud <zakariahammoud@email.com>
2022-11-09 12:55:32 +0200

    initial commit

---
+++ OddEvenNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OddEvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    
+    }
+}
